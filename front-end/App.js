959bf8f9752ad9002802a91910c64d35a86cd145
dangcaptkd2 <62542082+dangcaptkd@users.noreply.github.com>
2021-07-14 15:58:29 +0700

    add app.js

--- front-end/App.js
+++ front-end/App.js
@@ -1,94 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform, TextInput  } from 'react-native';
+import React, {useCallback, useState, useEffect } from 'react';
+import { Alert, Button, Image, View, Platform, Text } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 export default function ImagePickerExample() {
-//////////////////////////////////
-    // const [ title, setTitle ] = useState("");
-    // const [ cover, setCever ] = useState();
-    // //const [text, onChangeText] = React.useState("Useless Text");
 
-    // const newBook = () => {
-    //     const uploadData = new FormData();
-    //     uploadData.append('title', title);
-    //     uploadData.append('cover', cover);
-    //     console.log('cover', cover);
-    //     fetch('http://127.0.0.1:8000/books/', {
-    //     method: 'POST',
-    //     body: uploadData
-    //     })
-    //     .then( res => console.log('res:', res))
-    //     .catch(error => console.log(error))
-        
-    // }
-//////////////////////////////////////
     const [image, setImage] = useState(null);
+    const [caption, setCap] = useState(null);
 
-    useEffect(() => {
-        (async () => {
-        if (Platform.OS !== 'web') {
-            const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-            if (status !== 'granted') {
-            alert('Sorry, we need camera roll permissions to make this work!');
+    const pickImage = useCallback(async () => {
+        try {
+            const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+            console.log('granted', granted)
+            if (!granted) {
+                Alert.alert('Error', 'Sorry, we need camera roll permissions to make this work!');
+                return
             }
-        }
-        })();
-    }, []);
-
-    const pickImage = async () => {
-        var result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
-        base64: true,
-        });
-
-        
-        if (!result.cancelled) {
-            // console.log('result.url', result.uri);
-             setImage(result.uri);
-
-            // setTitle('xxx');
-            // setCever(result.uri);
-            // newBook();
-        
-            let localUri = result.uri;
-            let filename = localUri.split('/').pop();
-            // Infer the type of the image
-            let match = /\.(\w+)$/.exec(filename);
-            let type = match ? `image/${match[1]}` : `image`;
-              
-            console.log('type:', type);
-
-            const uploadData = new FormData();
-            uploadData.append('title', 'lan3');
+    
+            var result = await ImagePicker.launchCameraAsync({
+                allowsEditing: false,
+                aspect: [4, 3],
+                quality: 1,
+            });
+    
+            if (!result.cancelled) {
+                setImage(result.uri);
             
-            uploadData.append('cover', result.uri);
-            //uploadData.append('cover', { uri: localUri, name: filename, type });
+                let localUri = result.uri;
+                // Infer the type of the image
 
-            fetch('http://127.0.0.1:8000/books/', {
-            method: 'POST',
-            body: uploadData
-            })
-            .then( res => console.log('res:', res))
-            .catch(error => console.log(error))
+                const uploadData = new FormData();
+                uploadData.append('title', 'image');
         
+                if (Platform.os !== 'web') {
+                    uploadData.append('cover', { name: 'cover.jpg', type: 'image/jpg', uri: localUri });
+                } else {
+                    uploadData.append('cover', localUri, 'cover.png');                
+                }
+                const res = await fetch('http://f2a835d85dc2.ngrok.io/books/', {
+                    method: 'POST',
+                    body: uploadData,
+                    headers: {
+                        'Content-Type': 'multipart/form-data; ',
+                    },
+                })
+                const data = await res.json() //  res.text();
+                console.log('res:', data)
+            }
+        } catch (e) {
+            console.log('eee', e)
         }
-            
-            //console.log('helllo:', result);
-        };
-        
 
+        const res = await fetch('http://f2a835d85dc2.ngrok.io/get_caption/', {
+                        method: 'GET',
+                    })
+        const data = await res.json() //  res.text();
+        console.log('res:', data)
+        setCap(data.caption)
 
+        }, []);
+    
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>  
-        {/* <TextInput
-        onChangeText={(evt) => setTitle(evt.target.value)}
-        value={title}
-        /> */}
+       
       <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      {image && <Image source={{ uri: image }} style={{ width: 350, height: 350 }} />}
+      
+      
+      {caption && <Text>{caption} </Text> }
+
     </View>
   );
 };
